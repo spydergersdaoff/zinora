cf1aa1bc289793cc4ddf20dd64f7d3cea05d1ff8
SpyderGersda <160939622+spydergersdaoff@users.noreply.github.com>
2026-06-06 18:49:57 +0200

    Update script URL for PlaceId 76460552000865

--- loader.lua
+++ loader.lua
@@ -1,7 +1,7 @@
 local PlaceId = game.PlaceId
 
 local idsJeux = {
-    [76460552000865] = 'loadstring(game:HttpGet("https://pastefy.app/AU3dcP9Y/raw"))()',
+    [76460552000865] = 'loadstring(game:HttpGet("https://pastefy.app/KTjnzyRB/raw"))()',
     [142823291] = 'loadstring(game:HttpGet("https://raw.smokingscripts.org/vertex.lua"))()',
 }
 
